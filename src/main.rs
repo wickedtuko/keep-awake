676e045aeaeb76f0f8545cc2186c82454008bed5
tuko <tuko.ustx@gmail.com>
2024-10-26 09:48:31 -0500

    Show message - it keeps it running

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::u32;
 use winapi::um::winuser::{mouse_event, MOUSEEVENTF_MOVE};
 
 fn main() {
+	print!("This application keeps the machine awake!\n");
 	loop {
 		unsafe {
 			mouse_event(MOUSEEVENTF_MOVE, 0, 1, 0, 0);
